d514295569e3e21a4c8281a84cc2ec68ba2b4a56
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-14 12:58:32 +0000

    Feat: Stone Age Completed

---
+++ Stone Age/execute.sh
@@ -0,0 +1,10 @@
+#sudo apt-get update
+#sudo apt-get install -y fp-compiler
+
+cd src
+
+ls
+
+fpc markings.pas
+./markings
+rm markings markings.o
---
+++ Stone Age/src/markings.pas
@@ -0,0 +1,17 @@
+program markings;    
+
+begin
+
+
+      writeln('Optio quisquam explicabo voluptatum odio. Ab blanditiis alias sit maiores cupiditate ducimus est vero. Officia sint maiores dolor modi natus perferendis.');
+   writeln('21f'); // this is a hex 
+  
+(* This is a binary file *)  
+
+    writeln(10110110111);
+
+    
+    {  This is a int }  
+    writeln('248'); // this is an int 
+  
+    end.
