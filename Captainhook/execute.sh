a3789854c4892feba02b9b9af77cabeb7aca27b7
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-19 17:22:00 +0000

    test commit - pjuoj

--- Captainhook/execute.sh
+++ Captainhook/execute.sh
@@ -8,25 +8,12 @@ HOOK_FILE=".git/hooks/commit-msg"
 
 cat << 'EOF' > "$HOOK_FILE"
 #!/bin/bash
-if [ ! -d "./out" ]; then
-  echo "Creating ./out directory..."
-  mkdir ./out
-fi
 
-if [ ! -f "./out/commits.txt" ]; then
-  echo "Creating ./out/commits.txt file..."
-  touch ./out/commits.txt
-fi
+mkdir ./out
+echo "" >> ./out/commits.txt
 
-# Ensure the ./out directory exists, create it if not
-if [ ! -d "./out" ]; then
-  mkdir -p ./out
-fi
 
-# Ensure the ./out/commits.txt file exists, create it if not
-if [ ! -f "./out/commits.txt" ]; then
-  touch ./out/commits.txt
-fi
+
 
 # Append the commit message to ./out/commits.txt
 cat "$1" >> ./out/commits.txt
