d2d7af2cf71ac6e58593ed92c3bae24593460e43
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-19 18:01:10 +0000

    test commit - 7i5xqr

--- Captainhook/execute.sh
+++ Captainhook/execute.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # Initialize git repository if not already initialized
-if [ ! -d ".git" ]; then
-  git init
-fi
-
-# Ensure the ./out directory exists, create it if not
-if [ ! -d "./out" ]; then
-  mkdir -p ./out
-fi
-
-# Ensure the ./out/commits.txt file exists, create it if not
-if [ ! -f "./out/commits.txt" ]; then
-  touch ./out/commits.txt
-fi
-
+git init
 # Set up the pre-commit hook
 HOOK_FILE=".git/hooks/pre-commit"
 
