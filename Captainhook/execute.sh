a66dd3bb107007b5e2098accab129d1683d477e5
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-19 16:17:40 +0000

    test commit - ckg16

--- Captainhook/execute.sh
+++ Captainhook/execute.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
-# List git commits in single line format and pipe to log file
-echo "Piping commits to log file"
-git log --pretty=format:'%s' > ./out/commits.log
+
+# Function to check if only bash files are present
+check_bash_files() {
+  for file in *; do
+    if [[ ! -d "$file" && "${file##*.}" != "sh" ]]; then
+ 
+      exit 1
+    fi
+  done
+}
+
+# Function to check if a git repository is initialized
+check_git_repo() {
+  if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+
+    exit 1
+  fi
+}
+
+# Function to log git commits to out/commits.txt
+log_git_commits() {
+  mkdir -p out
+  git log --pretty=format:"%s" > ./out/commits.txt
+  if [[ $? -ne 0 ]]; then
+    
+    exit 1
+  fi
+}
+
+
+check_bash_files
+check_git_repo
+log_git_commits
+
