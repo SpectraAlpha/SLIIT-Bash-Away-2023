23fb0a271a8a493ce8a2ab34b53137f59175247c
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-19 16:14:47 +0000

    test commit - uqbb1i

--- Captainhook/execute.sh
+++ Captainhook/execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # List git commits in single line format and pipe to log file
 echo "Piping commits to log file"
-git log --pretty=format:'%h was %an, %ar, message: %s' > ./log/commits.log
+git log --pretty=format:'%h was %an, %ar, message: %s' > ./out/commits.log
