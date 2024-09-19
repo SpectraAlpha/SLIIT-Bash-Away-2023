78fc6e1100cbd49cfe54dbda1a958ed07ddeb6c8
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-19 16:59:29 +0000

    test commit - cr4dt6

--- Captainhook/execute.sh
+++ Captainhook/execute.sh
@@ -2,14 +2,14 @@
 
 # Function to set up git hook
 setup_git_hook() {
-    HOOK_DIR=".git/hooks"
+    HOOK_DIR="git/hooks"
     HOOK_FILE="$HOOK_DIR/post-commit"
 
     mkdir -p "$HOOK_DIR"
     cat << 'EOF' > "$HOOK_FILE"
 #!/bin/bash
 mkdir -p out
-git log --pretty=format:"%s" > ./out/commits.txt
+git log --pretty=format:"%s" > /out/commits.txt
 EOF
 
     chmod +x "$HOOK_FILE"
