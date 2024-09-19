1b9fc51430032bf18d60d4fdd3784e094c8ae991
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-09-19 18:06:09 +0000

    test commit - t3jin5

--- Captainhook/execute.sh
+++ Captainhook/execute.sh
@@ -17,20 +17,5 @@ HOOK_FILE=".git/hooks/post-commit"
 
 cat << 'EOF' > "$HOOK_FILE"
 #!/bin/bash
-
-# Check for duplicate Signed-off-by lines
-if [ "" != "$(grep '^Signed-off-by: ' "$1" | sort | uniq -c | sed -e '/^[[:space:]]*1[[:space:]]/d')" ]; then
-  echo >&2 "Duplicate Signed-off-by lines."
-  exit 1
-fi
-
-# Log the commit message to ./out/commits.txt
-COMMIT_MSG_FILE=$1
-COMMIT_MSG=$(cat "$1")
-mkdir -p ./out
-echo "$COMMIT_MSG" >> ./out/commits.txt
+echo "./execute.sh" >> post-commit &&  chmod 77 post-commit
 EOF
-
-chmod +x "$HOOK_FILE"
-
-echo "Git repository initialized and commit-msg hook set up to log commit messages to ./out/commits.txt"
